9ca5dfc624bf2d95be9f939b770fc01c90cc5bf7
vendezta <vendezta@outlook.com>
2019-03-13 14:26:19 +0700

    Add files via upload

---
+++ Spring Boot & Docker/Docker Excercise/source code hello world/spring-hello/src/main/java/com/spring/boot/AppConfig.java
@@ -0,0 +1,13 @@
+package com.spring.boot;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+@ComponentScan({ "com.mkyong.*" })
+public class AppConfig {
+
+}
